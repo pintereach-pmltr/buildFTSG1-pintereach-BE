059bbfee6454bcab3858ff0652cae340ad3633a8
erica-y-chen <eyufanchen@gmail.com>
2019-05-21 12:20:30 -0700

    added auth router tests

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -1,6 +1,77 @@
-const auth = require ('./auth-router.js')
-const request = require('supertest');
 
-describe('POST Register /', () => {
-    it('should return ')
+const request = require('supertest');
+const server = require('../api/server');
+const db = require('../database/dbConfig');
+
+
+afterAll(() => {
+    return db('users').truncate();
+  });
+
+
+describe('authorization', () => {
+    describe('POST /register', () => {
+        it('should register with status 201', () => {
+            const newUser = {
+                username: "test",
+                password: "password"
+            }
+
+            return request(server)
+            .post('api/auth/register')
+            .send(newUser)
+            .then(res => {
+                expect(201)
+            });
+
+        })
+
+        it('should register with status 422', () => {
+            const newUser = {
+                username: "",
+                password: "password"
+            }
+
+            return request(server)
+            .post('/register')
+            .send(newUser)
+            .then(res => {
+                expect(422)
+            });
+        })
+    })
+
+    describe('POST /login', () => {
+        it('should login with status 200', () => {
+            const newUser = {
+                username: "test",
+                password: "password"
+            }
+
+            return request(server)
+            .post('/login')
+            .send(newUser)
+            .then(res => {
+                expect(200)
+            });
+
+        })
+
+        it('should login with status 400', () => {
+            const newUser = {
+                username: "admin",
+                password: "password"
+            }
+
+            return request(server)
+            .post('/login')
+            .send(newUser)
+            .then(res => {
+                expect(400)
+            });
+
+        })
+    })
 })
+
+
--- users/users.spec.js
+++
@@ -1,6 +0,0 @@
-// const Users = require ('./users-router.js')
-// const request = require('supertest');
-
-// describe('GET /', () => {
-//     it('')
-// })
