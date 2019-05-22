ac616c02ea6e9be5dd47073c7b929f66ac000716
erica-y-chen <eyufanchen@gmail.com>
2019-05-22 11:02:13 -0700

    edited endpoints for tests because they weren't passing correctly

---
+++ users/users.spec.js
@@ -0,0 +1,15 @@
+const request = require('supertest')
+const server = require('../api/server')
+
+describe('user routes', () => {
+    describe('getAll', () => {
+        it('should give return an error when request is not authenticated with a token', () => {
+            return request(server)
+                .get('/api/users')
+                .then(res => {
+                    expect(res.status).toBe(401);
+                });
+        });
+    });
+
+})
