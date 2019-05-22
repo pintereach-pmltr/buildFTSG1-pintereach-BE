553a7fb71c3370749f67f3906e5ec6684e801f9b
Leland R <42158951+ltrii@users.noreply.github.com>
2019-05-22 13:23:35 -0700

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -44,10 +44,10 @@ module.exports = {
       max: 10
     },
     migrations: {
-      directory: "./data/migrations"
+      directory: "./database/migrations"
     },
     seeds: {
-      directory: "./data/seeds"
+      directory: "./database/seeds"
     }
   }
   
