659f496b13110cbe07857f7765bdf8efb31b9968
erica-y-chen <eyufanchen@gmail.com>
2019-05-23 09:31:22 -0700

    Added tests for boards

--- boards/boards.spec.js
+++ boards/boards.spec.js
@@ -1,2 +1,19 @@
 const request = require('supertest');
-const Server = require('../api/server');
+const boards = require('./boards-model')
+
+// describe('getAllUserBoards', () => {
+//     it('should return all boards for that user id ', () => {
+//       return boards.getAllUserBoards(1).then(res =>{
+//           expect(res.data.length).toBeGreaterThan(0);
+//       });
+//     });
+//   });
+
+describe('delete function', () => {
+    it('should delete the board by board id', () => {
+        boards.remove(1);
+        return boards.findById(1).then(res => {
+            expect(res).toBe(undefined)
+        })
+    })
+})
