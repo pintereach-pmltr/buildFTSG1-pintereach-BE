bef9acfeeefcec0945e8a904ec4c580a3c22cf31
Leland R <42158951+ltrii@users.noreply.github.com>
2019-05-23 23:46:59 -0700

    Update users-router.js
    User update endpoint

--- users/users-router.js
+++ users/users-router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const Users = require('../users/users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
+const db = require("../database/dbConfig");
 
 router.get('/', restricted, (req, res) => {
   Users.find()
@@ -17,6 +18,27 @@ router.get('/:id', restricted, (req, res) => {
       res.json(user);
     })
     .catch(err => res.send(err));
+});
+
+router.put("/:id", restricted, async (req, res) => {
+  try {
+    const count = await db("users")
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const user = await db("users")
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ error: "user not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
